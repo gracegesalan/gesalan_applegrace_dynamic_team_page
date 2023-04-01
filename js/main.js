f6dde779cbb11012c4b602240f923a2fc27c827a
gracegesalan <113370993+gracegesalan@users.noreply.github.com>
2023-04-01 10:47:22 -0400

    Edited the code and added the ReadMe file

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-// const h3 = document.getElementById("name1");
-// const caption1 = document.getElementById("caption1");
-
-// caption1.style.display = "none";
-
-// function toggleList() {
-//     if (caption1.style.display === "none") {
-//         caption1.style.display = "block";
-//     } else {
-//         caption1.style.display = "none";
-//     }
-// }
-
-// h3.addEventListener("click", toggleList);
 
 
 let member = {
@@ -35,7 +21,8 @@ function loadHeroData() {
     lightbox.querySelector(".name").textContent = member[this.dataset.member].name;
     lightbox.querySelector(".bio").textContent = member[this.dataset.member].bio;
 
-    // heroes[this.datase.member].headline 
+    lightbox.style.display = 'block';
+    
 }
 
 
